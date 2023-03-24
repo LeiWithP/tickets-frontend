3162e1833178c8813c4925b8b769932d7148286c
LeiWithP <pedro.barrita1029@gmail.com>
2023-03-23 20:12:19 -0600

    Login made pretty

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  mode: "jit",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: "#00040f",
-        secondary: "#00f6ff",
-        dimWhite: "rgba(255, 255, 255, 0.7)",
-        dimBlue: "rgba(9, 151, 124, 0.1)",
+        primary: '#289A96',
+        secondary: '#144A46',
+        tertiary: '#606060',
+        extra: '#FC6459',
       },
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
-    screens: {
-      xs: "480px",
-      ss: "620px",
-      sm: "768px",
-      md: "1060px",
-      lg: "1200px",
-      xl: "1700px",
+      backgroundImage: {
+        'random-image': "url('https://source.unsplash.com/random')",
+      }
     },
   },
   plugins: [],
-};
+}
