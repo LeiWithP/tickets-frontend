7f9093bd6df3d921c9ebb055e9fa5dc3b0684b74
LeiWithP <pedro.barrita1029@gmail.com>
2023-04-04 13:55:33 -0600

    Template Tickets done

--- src/pages/Creativo/Campanas.jsx
+++ src/pages/Creativo/Campanas.jsx
@@ -5,53 +5,138 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 const tareasIniciales = [
   {
     id: "1",
-    text: "Ticket 1",
-    info: "realizar funcionamiento de cartas",
+    peticion: "Ticket 1",
+    medio_origen: "Correo",
+    fecha_limite: "06/04/2023",
+    servidor_ubicacion: "",
+    actividad: "Web",
+    uso: "Actualizacion de Datos",
+    // frecuencia: "",
+    // duracion: "",
+    estado: "Correcciones",
+    fecha_entrega: "",
+    info_cliente: "Informacion del cliente",
+    observaciones: "",
+    correcciones: "",
+    error: "",
+    tipo_error: "",
+    empresa: "Gaytan",
+    levanta_ticket: "Maria",
+    cliente_solicita: "Roberto G.",
+    encargado: "Maria",
+    apoyo: "Mario",
   },
   {
     id: "2",
-    text: "Ticket 2",
-    info: "realizar funcionamiento de cartas",
+    peticion: "Ticket 2",
+    medio_origen: "Correo",
+    fecha_limite: "06/04/2023",
+    servidor_ubicacion: "",
+    actividad: "Web",
+    uso: "Actualizacion de Datos",
+    // frecuencia: "",
+    // duracion: "",
+    estado: "Correcciones",
+    fecha_entrega: "",
+    info_cliente: "Informacion del cliente",
+    observaciones: "",
+    correcciones: "",
+    error: "",
+    tipo_error: "",
+    empresa: "Gaytan",
+    levanta_ticket: "Maria",
+    cliente_solicita: "Roberto G.",
+    encargado: "Maria",
+    apoyo: "Mario",
   },
   {
     id: "3",
-    text: "Ticket 3",
-    info: "realizar funcionamiento de cartas",
+    peticion: "Ticket 3",
+    medio_origen: "Correo",
+    fecha_limite: "06/04/2023",
+    servidor_ubicacion: "",
+    actividad: "Web",
+    uso: "Actualizacion de Datos",
+    // frecuencia: "",
+    // duracion: "",
+    estado: "Correcciones",
+    fecha_entrega: "",
+    info_cliente: "Informacion del cliente",
+    observaciones: "",
+    correcciones: "",
+    error: "",
+    tipo_error: "",
+    empresa: "Gaytan",
+    levanta_ticket: "Maria",
+    cliente_solicita: "Roberto G.",
+    encargado: "Maria",
+    apoyo: "Mario",
   },
 ];
 
 const initialUsers = [
   {
     id: "1",
-    nombre: "Mario",
+    username: "Mario",
+    first_name: "Mario Hugo",
+    last_name: "Mendez Villa",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-02-2023 13:02",
   },
   {
     id: "2",
-    nombre: "Maria",
+    username: "Maria",
+    first_name: "Maria Agata",
+    last_name: "Rodriguez Gomez",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-04-2023 13:45",
   },
   {
     id: "3",
-    nombre: "Hugo",
+    username: "Roberto",
+    first_name: "Roberto",
+    last_name: "Rodriguez Gomez",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-04-2023 13:45",
   },
   {
     id: "4",
-    nombre: "Roberto",
+    username: "Emily",
+    first_name: "Emily Helena",
+    last_name: "Rodriguez Gomez",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-04-2023 13:45",
   },
   {
     id: "5",
-    nombre: "Helena",
+    username: "Emily",
+    first_name: "Emily Helena",
+    last_name: "Rodriguez Gomez",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-04-2023 13:45",
   },
   {
     id: "6",
-    nombre: "Sofia",
+    username: "Emily",
+    first_name: "Emily Helena",
+    last_name: "Rodriguez Gomez",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-04-2023 13:45",
   },
   {
     id: "7",
-    nombre: "Alex",
-  },
-  {
-    id: "8",
-    nombre: "Mindy",
+    username: "Emily",
+    first_name: "Emily Helena",
+    last_name: "Rodriguez Gomez",
+    group: "creativo",
+    is_active: "true",
+    last_login: "28-04-2023 13:45",
   },
 ];
 
@@ -83,9 +168,9 @@ const Campanas = () => {
     )
       return;
 
-    if (result.destination.droppableId === "users")
-      setusers((prevUsers) =>
-        reorder(prevUsers, source.index, destination.index)
+    if (result.destination.droppableId === "tickets")
+      setTickets((prevTickets) =>
+        reorder(prevTickets, source.index, destination.index)
       );
     if (result.destination.droppableId === "1")
       setusers((prevUsers) => deleteCard(prevUsers, source.index));
@@ -103,37 +188,17 @@ const Campanas = () => {
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
       <div className="h-full p-1 bg-slate-100 flex">
-        <div className="h-full w-1/4 m-1 p-2 bg-gradient-to-b from-primary flex-col rounded-md">
+        <div className="h-full w-1/4 m-1 p-2 bg-primary flex-col rounded-md">
           <h2 className="m-2 text-center font-bold">Tickets</h2>
           <div>
-            {/* <Droppable droppableId="tickets">
-              {(droppableProvided) => ( */}
-            <ul
-            // {...droppableProvided.droppableProps}
-            // ref={droppableProvided.innerRef}
-            >
-              {tickets.map((ticket) => (
-                <TicketBit ticket={ticket} />
-              ))}
-              {/* {droppableProvided.placeholder} */}
-            </ul>
-            {/* )} */}
-            {/* </Droppable> */}
-          </div>
-        </div>
-        <div className="h-full w-3/4 m-1 p-2 rounded-md bg-gradient-to-b from-secondary">
-          <h2 className="m-2 text-center">Creativos</h2>
-          <div>
-            <Droppable droppableId="users">
+            <Droppable droppableId="tickets">
               {(droppableProvided) => (
                 <ul
                   {...droppableProvided.droppableProps}
                   ref={droppableProvided.innerRef}
                 >
-                  {users.map((user, index) => (
-                    <li key={user.id}>
-                      <Usercard key={user.id} user={user} index={index} />
-                    </li>
+                  {tickets.map((ticket, index) => (
+                    <TicketBit key={ticket.id} ticket={ticket} index={index} />
                   ))}
                   {droppableProvided.placeholder}
                 </ul>
@@ -141,44 +206,77 @@ const Campanas = () => {
             </Droppable>
           </div>
         </div>
+
+        <div className="h-full w-3/4 m-1 rounded-md flex flex-col">
+          <div className="h-1/8 w-full rounded-md bg-secondary">
+            <h2 className="mt-2 pt-2 text-center font-bold">Creativos</h2>
+            <div className="px-2 overflow-auto">
+              <Droppable droppableId="users">
+                {(droppableProvided) => (
+                  <ul
+                    className="flex"
+                    {...droppableProvided.droppableProps}
+                    ref={droppableProvided.innerRef}
+                  >
+                    {users.map((user, index) => (
+                      <li key={user.id}>
+                        <Usercard key={user.id} user={user} index={index} />
+                      </li>
+                    ))}
+                    {droppableProvided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            </div>
+          </div>
+          <div className="w-full mt-2 p-2 rounded-md bg-secondary flex grow justify-center">
+            <p className="m-2 text-center self-center font-light">
+              Selecciona un Ticket o Usuario
+            </p>
+            <div></div>
+          </div>
+        </div>
       </div>
     </DragDropContext>
   );
 };
 
-const TicketBit = ({ ticket }) => {
+const TicketBit = ({ ticket, index }) => {
   return (
-    <Droppable droppableId={ticket.id}>
-      {(droppableProvided) => (
+    <Draggable draggableId={ticket.id} index={index}>
+      {(draggableProvided) => (
         <div
           className={`m-3 p-1 h-24 bg-white rounded-md hover:scale-95`}
-          {...droppableProvided.droppableProps}
-          ref={droppableProvided.innerRef}
+          ref={draggableProvided.innerRef}
+          {...draggableProvided.draggableProps}
+          {...draggableProvided.dragHandleProps}
         >
-          <p>{ticket.text}</p>
-          <p>{ticket.info}</p>
-          {droppableProvided.placeholder}
+          <p>{ticket.id}</p>
+          <p>{ticket.peticion}</p>
         </div>
       )}
-    </Droppable>
+    </Draggable>
   );
 };
 
 const Usercard = ({ user, index }) => {
   return (
-    <Draggable draggableId={user.id} index={index}>
-      {(draggableProvided) => (
-        <div
-          className="m-3 p-1 flex bg-white rounded-md hover:scale-95"
-          ref={draggableProvided.innerRef}
-          {...draggableProvided.draggableProps}
-          {...draggableProvided.dragHandleProps}
-        >
-          <AccountCircleIcon className="" />
-          <p>{user.nombre}</p>
-        </div>
-      )}
-    </Draggable>
+    // <Draggable draggableId={user.id} index={index}>
+    //   {(draggableProvided) => (
+    <div
+      className="m-3 p-1 flex bg-white rounded-md hover:scale-95 w-40"
+      // ref={draggableProvided.innerRef}
+      // {...draggableProvided.draggableProps}
+      // {...draggableProvided.dragHandleProps}
+    >
+      {/* <AccountCircleIcon className="" /> */}
+      <div className="bg-primary w-12 h-12 m-2 rounded-full"></div>
+      <div className="m-2">
+        <p>{user.first_name}</p>
+      </div>
+    </div>
+    //   )}
+    // </Draggable>
   );
 };
 
