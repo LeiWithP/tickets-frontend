f4a91c3e6a42aebc4835587702cf9ba1ca5a5b3d
Abby25CE <102004475+Abby25CE@users.noreply.github.com>
2023-04-27 12:56:01 -0600

    Update Parrillas.jsx

--- src/pages/General/Parrillas.jsx
+++ src/pages/General/Parrillas.jsx
@@ -19,115 +19,124 @@ const Tickets = () => {
 
 
   const handleCardClick = (buttonId, object) => {
-    setActiveButton(buttonId);
-    buttonId === "Parrillas"
-      ? setActiveParrilla(object)
-      : console.log("Id unknow");
+
+    if (activeButton == buttonId) {
+      setActiveButton("")
+      console.log(object)
+    }
+    else {
+      setActiveButton(buttonId)
+      buttonId === "Parrillas"
+        ? setActiveParrilla(object)
+        : console.log("Id unknow")
+      console.log("else")
+    }
   };
 
-  const handleDragEnd = (result) => {
-    const { source, destination } = result;
-    if (!result.destination) return;
-    if (
-      source.index === destination.index &&
-      source.droppableId === destination.droppableId
-    )
-      return;
 
-    if (result.destination.droppableId === "tickets")
-      setTickets((prevTickets) =>
-        reorder(prevTickets, source.index, destination.index)
-      );
-    // if (result.destination.droppableId === "1")
-    //   setusers((prevUsers) => deleteCard(prevUsers, source.index));
-    // const items = Array.from(tickets);
-    // const [reorderedItem] = items.splice(result.source.index, 1);
+const handleDragEnd = (result) => {
+  const { source, destination } = result;
+  if (!result.destination) return;
+  if (
+    source.index === destination.index &&
+    source.droppableId === destination.droppableId
+  )
+    return;
 
-    // if (result.destination.droppableId === "users") {
-    //   setCreativos((creativos) => [...creativos, reorderedItem]);
-    // } else {
-    //   items.splice(result.destination.index, 0, reorderedItem);
-    //   setTickets(items);
-    // }
-  };
+  if (result.destination.droppableId === "tickets")
+    setTickets((prevTickets) =>
+      reorder(prevTickets, source.index, destination.index)
+    );
+  // if (result.destination.droppableId === "1")
+  //   setusers((prevUsers) => deleteCard(prevUsers, source.index));
+  // const items = Array.from(tickets);
+  // const [reorderedItem] = items.splice(result.source.index, 1);
 
-  return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <TwoColSwitch
-        name="Tickets"
-        leftChild={
-          <>
-            <h2 className="m-2 text-center font-bold">Tickets</h2>
-            <div>
-              <Droppable droppableId="tickets">
-                {(droppableProvided) => (
-                  <ul
-                    {...droppableProvided.droppableProps}
-                    ref={droppableProvided.innerRef}
-                    className="flex flex-col"
-                  >
-                    {parrillas.map((parrillas, index) => (
-                      <DraggableParrilla
-                        key={parrillas.id}
-                        onClick={() => handleCardClick("Parrillas", parrillas[0])}
-                        parrilla={parrillas}
-                        index={index}
-                      />
-                    ))}
-                    {droppableProvided.placeholder}
-                  </ul>
-                )}
-              </Droppable>
-            </div>
-          </>
-        }
-        rightChild={
-          // <TopBar
-          //   topChild={
-          //     <>
-          //       <h2 className="mt-2 pt-2 text-center font-bold">Creativos</h2>
-          //       <div className="px-2 overflow-auto snap-x scroll-p-2">
-          //         <Droppable droppableId="users">
-          //           {(droppableProvided) => (
-          //             <ul
-          //               className="flex"
-          //               {...droppableProvided.droppableProps}
-          //               ref={droppableProvided.innerRef}
-          //             >
-          //               {users.map((user, index) => (
-          //                 <li key={user.id}>
-          //                   <Usercard
-          //                     onClick={() => handleCardClick("user", user)}
-          //                     key={user.id}
-          //                     user={user}
-          //                     index={index}
-          //                   />
-          //                 </li>
-          //               ))}
-          //               {droppableProvided.placeholder}
-          //             </ul>
-          //           )}
-          //         </Droppable>
-          //       </div>
-          //     </>
-          //   }
-          //   BodyChild={
-          activeButton === "Parrillas" ? (
-            <TablasParrillas parrillas={parrillas} />
-          ) : (
-            <p className="m-2 text-center self-center font-light">
-              Selecciona un Ticket o Usuario
-            </p>
-          )
-        }
-        swap={
+  // if (result.destination.droppableId === "users") {
+  //   setCreativos((creativos) => [...creativos, reorderedItem]);
+  // } else {
+  //   items.splice(result.destination.index, 0, reorderedItem);
+  //   setTickets(items);
+  // }
+};
+
+return (
+  <DragDropContext onDragEnd={handleDragEnd}>
+    <TwoColSwitch
+      name="Tickets"
+      leftChild={
+        <>
+          <h2 className="m-2 text-center font-bold">Tickets</h2>
+          <div>
+            <Droppable droppableId="tickets">
+              {(droppableProvided) => (
+                <ul
+                  {...droppableProvided.droppableProps}
+                  ref={droppableProvided.innerRef}
+                  className="flex flex-col"
+                >
+                  {parrillas.map((parrillas, index) => (
+                    <DraggableParrilla
+                      key={parrillas.id}
+                      onClick={() => handleCardClick("Parrillas", parrillas[0])}
+                      parrilla={parrillas}
+                      index={index}
+                    />
+                  ))}
+                  {droppableProvided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+          </div>
+        </>
+      }
+      rightChild={
+        // <TopBar
+        //   topChild={
+        //     <>
+        //       <h2 className="mt-2 pt-2 text-center font-bold">Creativos</h2>
+        //       <div className="px-2 overflow-auto snap-x scroll-p-2">
+        //         <Droppable droppableId="users">
+        //           {(droppableProvided) => (
+        //             <ul
+        //               className="flex"
+        //               {...droppableProvided.droppableProps}
+        //               ref={droppableProvided.innerRef}
+        //             >
+        //               {users.map((user, index) => (
+        //                 <li key={user.id}>
+        //                   <Usercard
+        //                     onClick={() => handleCardClick("user", user)}
+        //                     key={user.id}
+        //                     user={user}
+        //                     index={index}
+        //                   />
+        //                 </li>
+        //               ))}
+        //               {droppableProvided.placeholder}
+        //             </ul>
+        //           )}
+        //         </Droppable>
+        //       </div>
+        //     </>
+        //   }
+        //   BodyChild={
+        activeButton === "Parrillas" ? (
           <TablasParrillas parrillas={parrillas} />
-        }
-      />
-      {/* }
+        ) : (
+          <p className="m-2 text-center self-center font-light">
+            Selecciona un Ticket o Usuario
+          </p>
+        )
+      }
+      swap={
+        <TablasParrillas parrillas={parrillas} />
+      }
+    />
+    {/* }
       /> */}
-    </DragDropContext>
-  );
+  </DragDropContext>
+);
 };
 
 export default Tickets;
